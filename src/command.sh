c4e0c7987e92418cf66fb9ac74c425b933276236
Marica Lucian <maricalucian@gmail.com>
2025-01-14 15:52:31 +0200

    bip

--- src/command.sh
+++ src/command.sh
@@ -2,4 +2,4 @@
 #test
 #tail -f test/output.log | grep -E "num_sanitized_ok.*id=2000|id=2000.*num_sanitized_ok"
 
-journalctl -ocat -u solana | grep -E "num_sanitized_ok.*id=2000|id=2000.*num_sanitized_ok"
+journalctl -focat -u solana | grep -E "num_sanitized_ok.*id=2000|id=2000.*num_sanitized_ok"
